dc3ec65278b7b6f19e7d12d70f2e5cd83ca2e36e
wilsaj <wilson.andrew.j+github@gmail.com>
2014-09-16 09:17:45 -0700

    add basic test

---
+++ test/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var assert = require('assert');
+var Metalsmith = require('metalsmith');
+var rimraf = require('rimraf');
+
+var each = require('..');
+
+
+describe('metalsmith-each', function(){
+  after(function(done){
+    rimraf('test/fixtures/basic/build', function () {
+      done();
+    });
+  });
+
+  it('works with basic assignment', function(done) {
+    var testCount = 0;
+    var expected = {
+      'index.md': 'INDEX.MD',
+      'nested/index.md': 'NESTED/INDEX.MD'
+    };
+
+    Metalsmith('test/fixtures/basic')
+      .use(each(function (file, filename) {
+        file.uppered = filename.toUpperCase();
+      }))
+      .use(function(files, metalsmith, done){
+        Object.keys(files).forEach(function(filename){
+          var file = files[filename];
+          var expectedName = expected[filename];
+          assert.equal(file.uppered, expectedName);
+          testCount++;
+        });
+        done();
+      })
+      .build(function(err){
+        if (err) {return done(err);}
+        assert.equal(testCount, Object.keys(expected).length);
+        done();
+      });
+  });
+});
